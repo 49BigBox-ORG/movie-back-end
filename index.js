9168711b5768b6e8f80cabcfdec0a8a79a32b2bc
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-07 21:13:32 +0700

    remove comment

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const app = express();
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema');
+const express = require('express')
+const app = express()
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema')
 
-const port = 3000;
+const port = 3000
 
-app.use(express.json());
-app.use('/api', graphqlHTTP({
+app.use(express.json())
+app.use(
+    '/api',
+    graphqlHTTP({
         schema: schema,
-        graphiql: true
+        graphiql: true,
     })
-);
+)
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
 
-module.exports = app;
+module.exports = app
