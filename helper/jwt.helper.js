0a6072d47bb33badb63151e54b1cad63385a5eb0
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-07 13:32:07 +0700

    add jwt

---
+++ helper/jwt.helper.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRED_TIME});
+}
+
+const decodeToken = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    }catch (e) {
+        return null;
+    }
+}
+
+const checkAuthen = (req, res, next) => {
+    const token = req.headers['x-access-token'];
+    const decoded = decodeToken(token)
+    if (decoded) {
+        next()
+    }else {
+        res.status(401).send('Unauthorized')
+    }
+}
+
+module.exports = {
+    generateToken,
+    decodeToken,
+    checkAuthen
+}
+
