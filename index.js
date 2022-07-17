9c1a9f3a97f7bfb367886b67bfe5d7751d719b1b
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-18 01:00:12 +0700

    enable the cross origin requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
@@ -12,6 +13,8 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
+
+app.use(cors({origin: true}))
 
 app.use(
     '/api',
