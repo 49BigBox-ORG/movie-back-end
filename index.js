9fa9f56faf28a60f2150c1453ad2dd83b090e825
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-20 19:01:07 +0700

    test deploy to vercel
    - delete header, modify middleware

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
-const allowCors = require('./middleware/allowCors')
+// const allowCors = require('./middleware/allowCors')
 
 const corsOptions = {
     origin: '*',
@@ -17,16 +17,26 @@ app.use(express.json())
 
 app.use(cors(corsOptions))
 
-// const middleware = (req, res, next) => {
-//     res.setHeader({key:})
-//     res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept')
-//     next()
-// }
+const middleware = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+    }
+    next()
+}
 
 app.use(
     '/api',
-    // middleware,
-    allowCors,
+    middleware,
+    // allowCors,
     graphqlHTTP({
         schema: schema,
         graphiql: true,
