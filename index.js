9b3e75198c1bc836e3e73ee50fbe3a1e68365a3e
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-20 21:30:32 +0700

    test deploy to vercel
    - delete allowCors.js
    - remove unnecessary code

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
-// const allowCors = require('./middleware/allowCors')
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,
+//     optionSuccessStatus: 200,
+// }
 
 const port = 8080
 
 app.use(express.json())
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
-const middleware = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-    }
-    next()
-}
+// const middleware = (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     // another common pattern
+//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
+//     if (req.method === 'OPTIONS') {
+//         res.status(200).end()
+//     }
+//     next()
+// }
 
 app.use(
     '/api',
-    middleware,
-    // allowCors,
+    // middleware,
     graphqlHTTP({
         schema: schema,
         graphiql: true,
