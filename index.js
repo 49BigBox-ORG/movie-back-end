dc2bf0a5d9492c7bfd1c819ed38da8a170512393
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-21 00:00:01 +0700

    add cors middleware

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
+const {corsMiddleware} = require('./middleware/cors.middleware')
 const port = 3000
 
 app.use(express.json())
+
 app.use(
     '/api',
     graphqlHTTP({
         schema: schema,
         graphiql: true,
+        corsMiddleware,
         customFormatErrorFn: (error) => {
             return {
                 message: error.originalError.message || error.message,
---
+++ middleware/cors.middleware.js
@@ -0,0 +1,13 @@
+const corsMiddleware = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200).end()
+    }
+    next()
+}
+
+module.exports = {
+    corsMiddleware,
+}
