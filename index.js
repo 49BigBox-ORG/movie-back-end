1086a8d3112e5757410fd1d4a0266172dd4f22d5
DinhCongTri <dinhcongtri0905@gmail.com>
2022-07-21 00:11:39 +0700

    remove corsMiddleware (custom)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
 const schema = require('./schema')
-const {corsMiddleware, corsOptions} = require('./middleware/cors.middleware')
+const {corsOptions} = require('./middleware/cors.middleware')
 const port = 3000
 
 app.use(express.json())
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(
     '/api',
     cors(corsOptions),
-    corsMiddleware,
+    // corsMiddleware,
     graphqlHTTP({
         schema: schema,
         graphiql: true,
--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -2,18 +2,6 @@ const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200,
 }
-
-const corsMiddleware = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200).end()
-    }
-    next()
-}
-
 module.exports = {
-    corsMiddleware,
     corsOptions,
 }
