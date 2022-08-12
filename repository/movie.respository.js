070529fc8872cfdc634bb5a9b4bc1d523f088086
DinhCongTri <dinhcongtri0905@gmail.com>
2022-08-12 23:31:06 +0700

    change data update MovieStatus table

--- repository/movie.respository.js
+++ repository/movie.respository.js
@@ -221,7 +221,7 @@ const updateMovieBasic = async (input, accessToken) => {
                     releaseDate: new Date(releaseDate),
                     director,
                     movieStatus: {
-                        data: {
+                        update: {
                             status,
                         },
                     },
